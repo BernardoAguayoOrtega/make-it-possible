eb40ea7d0ee9c7247d62f299c92abe9547f0ef86
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-09 21:12:57 -0600

    add app to entry point

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
 // import render method
 import { render } from 'react-dom';
 
+// import app component
+import App from './components/App/index.jsx';
+
 // create root const
 const root = document.getElementById('root');
 
 // render the app
-render(<h1>Hey</h1>, root);
+render(<App />, root);
