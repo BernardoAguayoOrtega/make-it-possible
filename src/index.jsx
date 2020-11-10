4e90e1a3e0737318b466f22f311d7c2d473c605e
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-09 20:59:02 -0600

    change react stric mode

--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,4 @@ import { render } from 'react-dom';
 const root = document.getElementById('root');
 
 // render the app
-render(
-  <React.StrictMode>
-    <h1>Hey</h1>
-  </React.StrictMode>,
-  root
-);
+render(<h1>Hey</h1>, root);
