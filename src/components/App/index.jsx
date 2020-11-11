1032981f97443012bd10dbedecc3eea5fa1b96a8
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-10 18:49:29 -0600

    add with succes material ui core

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,12 @@
+import Button from '@material-ui/core/Button';
+
 const App = () => {
   return (
     <>
       <h1>I&apos;m the app</h1>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </>
   );
 };
