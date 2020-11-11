092f2b2b17d1b4efd691aaac051261fa19ce1cf4
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-10 18:40:51 -0600

    Create error bundury

---
+++ src/components/ErrorBoundary/index.jsx
@@ -0,0 +1,31 @@
+// import prop-types
+import PropTypes from 'prop-types';
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.node,
+  fallback: PropTypes.string,
+};
+
+ErrorBoundary.defaultProps = {
+  fallback: 'Something went wrong',
+};
