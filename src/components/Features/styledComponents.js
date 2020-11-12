faf184297fd6d1761d621f805d57d5d97403ddde
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-12 17:06:30 -0600

    add script to run dev server with react hot loader and without ot

---
+++ src/components/Features/styledComponents.js
@@ -0,0 +1,38 @@
+// import styled
+import styled from 'styled-components';
+
+// header container
+export const HeaderContainer = styled.header`
+  background-color: black;
+  display: flex;
+  height: 10vh;
+  justify-content: space-between;
+  padding: 1.5rem;
+
+  @media (max-width: 450px) {
+    align-items: center;
+    flex-direction: column;
+    height: 20vh;
+    justify-content: center;
+  }
+`;
+
+// header left side
+export const LeftSide = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+// header right side
+export const RightSide = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  width: 30%;
+
+  @media (max-width: 450px) {
+    width: 100%;
+  }
+`;
