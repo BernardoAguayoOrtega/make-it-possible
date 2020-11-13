e54daa64e7782b293811f600c9c85892babdd585
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-12 19:38:49 -0600

    fix style problem

--- src/components/Card/styledComponents.js
+++ src/components/Card/styledComponents.js
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 
 // CardContainer
 export const CardContainer = styled.div`
-  max-width: 35vw;
-  min-width: 35vw;
+  max-width: 40vw;
+  min-width: 40vw;
   margin: 1rem auto;
   background: black;
   color: white;
 
   @media (max-width: 450px) {
-    max-width: 50vw;
-    min-width: 50vw;
+    max-width: 80vw;
+    min-width: 80vw;
   }
 `;
