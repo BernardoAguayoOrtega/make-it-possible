2026f170adf5b46fcd84355389a2d93d43c1cba3
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-16 18:00:45 -0600

    finish footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,25 +2,33 @@
  * Footer component, shows general info to user like about, privacy policy, etc
  */
 import Logo from '../../components/Logo/index.jsx';
+import {
+  FooterContainer,
+  FooterInfo,
+  Copyright,
+  LogoContainer,
+} from './styledComponents';
 
 const Footer = () => (
-  <footer className="footer">
-    <Logo aria-label="logo" aria-required="true" className="footer__logo" />
-    <div className="footer__subtitle">
-      <a className="about-link" href="/about">
+  <FooterContainer>
+    <LogoContainer>
+      <Logo aria-label="logo" aria-required="true" />
+    </LogoContainer>
+    <FooterInfo>
+      <a href="/about">
         <h2>About</h2>
       </a>
-      <a className="policy-link" href="/Privacy_Policy">
+      <a href="/Privacy_Policy">
         <h2>Privacy Policy</h2>
       </a>
-      <a className="team-link" href="/team">
+      <a href="/team">
         <h2>Team</h2>
       </a>
-    </div>
-    <p aria-label="rights" aria-required="true" className="footer__comments">
-      2020 by Bernado Aguayo and Alvaro Masias
-    </p>
-  </footer>
+    </FooterInfo>
+    <Copyright aria-label="rights" aria-required="true">
+      <p>2020 by Bernado Aguayo and Alvaro Masias</p>
+    </Copyright>
+  </FooterContainer>
 );
 
 export default Footer;
--- src/components/Footer/styledComponents.js
+++ src/components/Footer/styledComponents.js
@@ -4,4 +4,49 @@ import styled from 'styled-components';
 // export footer container
 export const FooterContainer = styled.footer`
   background: black;
+  display: grid;
+  grid-template-areas:
+    'a a a'
+    'b b b'
+    'c c c';
+`;
+
+export const LogoContainer = styled.div`
+  grid-area: a;
+  margin: 0 auto;
+  padding: 1rem;
+`;
+
+export const FooterInfo = styled.div`
+  align-items: center;
+  display: flex;
+  grid-area: b;
+  justify-content: space-evenly;
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+
+  h2 {
+    font-weight: 400;
+  }
+
+  @media (max-width: 450px) {
+    flex-direction: column;
+    h2 {
+      padding: 1rem;
+    }
+  }
+`;
+
+export const Copyright = styled.div`
+  color: white;
+  grid-area: c;
+  margin: 0 auto;
+  padding: 2rem 1rem 0.5rem 1rem;
+
+  @media (max-width: 450px) {
+    text-align: center;
+  }
 `;
