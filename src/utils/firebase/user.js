867e815a4f80ea3f5967f3f1f28a5e2b7aabb53a
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-25 16:55:28 -0600

    set up firebase config to call firestore

---
+++ src/utils/firebase/user.js
@@ -0,0 +1,13 @@
+import { firestore } from './firebase';
+
+export const createUserDocument = async (user) => {
+  const docRef = firestore.doc(`/users/${user.uid}`);
+
+  const userProfile = {
+    uid: user.uid,
+    email: user.email,
+    name: user.displayName,
+  };
+
+  return docRef.set(userProfile);
+};
