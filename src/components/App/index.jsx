475b9af2e6155c2bba7055dc700d5e287a65d117
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-26 21:06:37 -0600

    REfactor the components to show user info

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -4,16 +4,16 @@
 import { hot } from 'react-hot-loader/root';
 import Home from '../../pages/Home/index.jsx';
 import Layout from '../Layout/index.jsx';
+import UserDashboard from '../../pages/UserDashboard/index.jsx';
 import '../../utils/firebase/firebase';
 import { Switch, Route } from 'react-router-dom';
-import Dashboard from '../Dashboard/index.jsx';
 
 const App = () => {
   return (
     <Layout>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/dash" component={Dashboard} />
+        <Route exact path="/dash" component={UserDashboard} />
       </Switch>
     </Layout>
   );
---
+++ src/pages/UserDashboard/index.jsx
@@ -0,0 +1,10 @@
+/**
+ * user dashboard to handle our goals
+ */
+import GoalInfo from '../../components/GoalInfo/index.jsx';
+
+const UserDashboard = () => {
+  return <GoalInfo />;
+};
+
+export default UserDashboard;
