86ea5f962db9e837144c45598d0d1663a4b15f2d
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-26 21:27:29 -0600

    Layout userdashboard set up

--- src/pages/UserDashboard/index.jsx
+++ src/pages/UserDashboard/index.jsx
@@ -2,9 +2,20 @@
  * user dashboard to handle our goals
  */
 import GoalInfo from '../../components/GoalInfo/index.jsx';
+import GoalsList from '../../components/GoalsList/index.jsx';
+import Chart from '../../components/Chart/index.jsx';
+import { Container, RightSide } from './styled';
 
 const UserDashboard = () => {
-  return <GoalInfo />;
+  return (
+    <Container>
+      <GoalsList />
+      <RightSide>
+        <GoalInfo />
+        <Chart />
+      </RightSide>
+    </Container>
+  );
 };
 
 export default UserDashboard;
