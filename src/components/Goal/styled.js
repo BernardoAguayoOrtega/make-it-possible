4243e324d8a72815cc993d7a2bfcc8deb6da49b6
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-27 12:13:24 -0600

    Edit styles of indiviual goals

--- src/components/Goal/styled.js
+++ src/components/Goal/styled.js
@@ -2,13 +2,17 @@ import styled from 'styled-components';
 
 export const GoalContainer = styled.div`
   background-color: white;
+  background-image: linear-gradient(black, black), linear-gradient(white, white);
+  background-size: 0 5px, auto;
+  background-repeat: no-repeat;
+  background-position: center bottom;
   border-radius: 5px;
   margin: 1rem 0.5rem;
   padding: 0.3rem;
+  transition: all 0.2s ease-out;
 
   &:hover {
-    box-shadow: 5px 5px 5px 5px #000000;
-    transition: 0.5s;
+    background-size: 100% 5px, auto;
   }
 `;
 
